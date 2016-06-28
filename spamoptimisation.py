facf279b7d28895a5b3862eba97ab39580360f61
Andrew Ewen <andy1633@hotmail.co.uk>
2016-06-29 00:25:48 +0100

    Fix algorithm not taking into account combinations with less than "item-limit" elements. Prettier output. Add argument passing using argparse Python standard library.

--- spamoptimisation.py
+++ spamoptimisation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from itertools import combinations_with_replacement
 import json
-import sys
+import argparse
 
 def total_price(combination):
     return sum([float(item["price"]) for item in combination])
@@ -11,21 +11,30 @@ def evaluate_combination(desired_price, combination):
     return abs(desired_price - total)
 
 def main():
-    desired_price = 99.5
-    json_file = "spammenu.json"
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--price", type=float, default=30.0)
+    parser.add_argument("--menu", type=str, default="spammenu.json")
+    parser.add_argument("--item-limit", type=int, default=8)
+    args = parser.parse_args()
+
+    desired_price = args.price
+    json_file = args.menu
+    item_limit = args.item_limit
     with open(json_file) as menu_json:
         menu = json.load(menu_json)
 
     results = []
-    combinations = list(combinations_with_replacement(menu, 10))
-    for comb in combinations:
-        results.append(comb)
+    for i in range(1, item_limit + 1):
+        for comb in combinations_with_replacement(menu, i):
+            results.append(comb)
 
     results.sort(key=lambda comb: evaluate_combination(desired_price, comb))
 
-    for combination in results[:10]:
+    for combination in results[:4]:
         print(total_price(combination))
-        print(combination)
+        items = ",\n".join(["Name: {}, Price: {}".format(item["name"], item["price"]) for item in combination])
+        print(items)
+        print("\n")
 
 if __name__ == '__main__':
     main()
